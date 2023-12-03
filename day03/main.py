dcba6bf8e3ee47790e831ff6932d98d8b457ea63
ben <75566095+31b4@users.noreply.github.com>
2023-12-03 14:35:42 +0100

    day03

---
+++ day03/main.py
@@ -0,0 +1,41 @@
+def find_connection(engine_schematic, row, col):
+    for i in range(row-1, row+2):
+        for j in range(col-1, col+2):
+            if 0 <= i < len(engine_schematic) and 0 <= j < len(engine_schematic[i]) and not engine_schematic[i][j].isdigit() and engine_schematic[i][j] != '.':
+                if engine_schematic[i][j] == '*': # part 2
+                    return (True, i, j)
+                return (True, -1,-1)
+    return (False, -1,-1)
+
+def solve(data):
+    p1 = 0
+    p2 = 0
+    stars = {}
+    for row in range(len(data)):
+        connected, i, j = False, -1, -1
+        num = ''
+        for col in range(len(data[row])):
+            if data[row][col].isdigit():
+                if not connected:
+                    connected, i, j = find_connection(data, row, col)
+                elif i == -1 and j == -1:
+                    _, i, j = find_connection(data, row, col)
+                num += data[row][col]
+            if not data[row][col].isdigit() or col == len(data[row]) - 1:
+                if connected:
+                    p1 += int(num)
+                    if (i,j) in stars:
+                        p2 += int(stars[(i,j)]) * int(num)
+                    elif i != -1 and j != -1:
+                        stars[(i,j)] = num
+                    connected = False
+                num = ''
+    return p1, p2
+
+if __name__ == '__main__':
+    data = open("input.txt").read().split("\n")
+    res = 0
+    p1, p2 = solve(data)
+
+    print("Part 1: ", p1)
+    print("Part 2: ", p2)
