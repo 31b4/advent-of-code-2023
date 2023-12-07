f84bced632bb72470d60141d91fb23238d5bc79f
ben <75566095+31b4@users.noreply.github.com>
2023-12-07 07:43:14 +0100

    day07

---
+++ day07/p1.py
@@ -0,0 +1,72 @@
+from collections import Counter
+
+data = open("input.txt").read().split("\n")
+data = [i.split() for i in data]
+data = [[i[0], int(i[1])] for i in data]
+
+def parse(hand):
+    hand = list(hand)
+    for i in range(len(hand)):
+        if hand[i] == 'T':
+            hand[i] = 10
+        elif hand[i] == 'J':
+            hand[i] = 11
+        elif hand[i] == 'Q':
+            hand[i] = 12
+        elif hand[i] == 'K':
+            hand[i] = 13
+        elif hand[i] == 'A':
+            hand[i] = 14
+        else:
+            hand[i] = int(hand[i])
+    return hand
+
+
+five = []
+four = []
+full = []
+three = []
+two = []
+one = []
+high = []
+
+for i, (cards, point) in enumerate(data):
+    hand = parse(cards)
+    cnt = Counter(hand)
+
+    if len(set(cnt)) == 1:  # Five of a kind
+        five.append((hand, i))
+    elif len(set(cnt)) == 5:  # High card
+        high.append((hand, i))
+    elif len(set(cnt)) == 2:  # four or full
+        for c in cnt:
+            if cnt[c] == 4:  # Four of a kind
+                four.append((hand, i))
+                break
+            elif cnt[c] == 3:  # Full house
+                full.append((hand, i))
+                break
+    elif len(set(cnt)) == 3:  # three or two
+        for c in cnt:
+            if cnt[c] == 3:  # Three of a kind
+                three.append((hand, i))
+                break
+            elif cnt[c] == 2:  # Two pair
+                two.append((hand, i))
+                break
+    elif len(set(cnt)) == 4:  # One pair
+        one.append((hand, i))
+
+five.sort()
+four.sort()
+full.sort()
+three.sort()
+two.sort()
+one.sort()
+high.sort()
+
+res = 0
+for i,x in enumerate(high+one+two+three+full+four+five):
+    res += data[x[1]][1] * (i+1)
+
+print("Part 1:",res)
